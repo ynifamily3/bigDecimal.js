a5ed293a37b18f1f3f82bca6ed5fce4db6d99112
jongkeun choi <ynifamily3@gmail.com>
2017-10-29 10:16:45 +0000

    implement rational adder
    Signed-off-by: jongkeun choi <ynifamily3@gmail.com>

--- bigDecimal.js
+++ bigDecimal.js
@@ -24,6 +24,7 @@ function BigDecimal(strExp) {
         if(strExp instanceof BigDecimal) {
               return strExp;
           } else {
+              //구문해석으로 값을 그대로 집어넣을수 있게 한다. {val:~,props:{~}}
               return new BigDecimal("0");
           }
         }
@@ -38,21 +39,51 @@ function BigDecimal(strExp) {
 BigDecimal.fn = BigDecimal.prototype = {
     init: function(Exp) {
         this.val = Exp;
-        this.prop = {
+        var prop = {
+            sign: 1,
+            integer : "0",
             numerator : "0",
             denominator : "1"
-        }; //dbg, this->var
-        parseNumber.call(this, Exp, this.prop); //dbg, this(2nd)->delete this.
+        };
+        //내부적인 값으로 사용하고 싶은데, 다른 오브젝트와 상호작용하려면 그 값이 필요한 모순?
+        this.getProp = function() { return prop; };
+        parseNumber.call(this, Exp, this.getProp());
     },
     toString: function() {
-        return this.val;
+        var props = this.getProp();
+        console.log(props);
+        return (props.sign == 0 ? "-":"") + props.integer + (props.numerator != 0 ? (" + " + props.numerator + "/" + props.denominator):"");
     },
     add: function(other) {
         if(other != 0 && !other) return this;
         if(typeof(other) === 'object' && other instanceof BigDecimal) {
-            return string_add(this.val, other.val);//dbg
+            var prop1 = this.getProp();
+            var prop2 = other.getProp();
+            var result = {prop:{}};
+            if(prop1.sign === prop2.sign) {
+                //부호 같음 상태
+                if(prop1.denominator.length === prop2.denominator.length) {
+                    //통분된 상태
+                    result.prop.sign = prop1.sign;
+                    result.prop.integer = string_add(prop1.integer, prop2.integer);
+                    result.prop.numerator = string_add(prop1.numerator, prop2.numerator);
+                    result.prop.denominator = prop1.denominator;
+                    if(0/*두 string 을 compare 필요*/) {
+                        //분모 0 붙이기
+                        //분자 분모만큼 빼기
+                        //정수부 증감
+                    }
+                }
+            }
         }
-        else return this.add(BigDecimal(other));
+        else return this.add(new BigDecimal(other));
+    },
+    sub: function(other) {
+        if(other != 0 && !other) return this;
+        if(typeof(other) === 'object' && other instanceof BigDecimal) {
+            return new BigDecimal(string_sub(this.val, other.val));//dbg
+        }
+        else return this.sub(new BigDecimal(other));
     }
 };
 
@@ -73,10 +104,13 @@ BigDecimal.extend({
 });
 
 function parseNumber(strExpNumber, prop) {
+    //1.0, sign,integer ~~~~~
     var re = /[+-]?(Infinity|NaN|\d+)/g;
     var container = strExpNumber.match(re);
-    if(!container || container.length == 0 || container.length > 2)
+    console.log(container);
+    if(!container || container.length == 0 || container.length > 2) {
         throw new Error("Unexpected Number : " + strExpNumber);
+    }
     
     if(container[0][0] == '-') prop.sign = 0;
     else prop.sign = 1;
@@ -85,8 +119,9 @@ function parseNumber(strExpNumber, prop) {
     else prop.integer = container[0];
     prop.integer = trim_zero_l(prop.integer);
     if(container.length == 2) {
-        prop.numerator = trim_zero_l(container[1].split("")).join("");
-        prop.denominator = "1"+"0".repeat(container[1].length);
+        prop.numerator = trim_zero_r(container[1].split("")).join("");
+        prop.denominator = "1"+"0".repeat(prop.numerator.length);
+        prop.numerator = trim_zero_l(prop.numerator.split("")).join("");
     }
 }
 
@@ -137,7 +172,6 @@ function string_add(strExp1, strExp2) {
 function string_sub(strExp1, strExp2) {
     //항상 strExp1 >= strExp2 임을 가정한다.
     var lptr = strExp1.length - 1;
-    var llen = lptr + 1;
     var rptr = strExp2.length - 1;
     var carry = 0;
     var n1 = strExp1.split("").map(function(x){return Number(x)});
