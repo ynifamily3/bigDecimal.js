dafaec71cfa96db2d1392005719f7bfe2acd2c1a
jongkeun choi <ynifamily3@gmail.com>
2017-12-04 03:03:20 +0000

    constructor

--- bigDecimal_new.js
+++ bigDecimal_new.js
@@ -3,9 +3,10 @@ var BD = {
     // Enums
     PLUS: 1,
     MINUS: 0
-}
+};
+
 function BigDecimal(strExp) {
-    strExp = !strExp ? "0" : strExp;
+    strExp = !strExp ? '0' : strExp;
     if(this === window) return new BigDecimal(strExp);
     var state = {
         _isNaN: false, //숫자가 아님
@@ -14,16 +15,56 @@ function BigDecimal(strExp) {
         circulating: false, // 순환소수 여부
         sign: BD.PLUS, // 부호
         integer: '0', // 정수부
-        denominator: '1', // 분모
+        denominator: 1, // 분모(의 길이)
         numerator: '0', // 분자
-        reduced_denominator: '1', // (통분된)분모
-        reduced_numerator: '0', // (통분된)분자
+        reduced_denominator: '1', // (통분된)분모 -> 순환소수 대응
+        reduced_numerator: '0', // (통분된)분자 -> 순환소수 대응
     };
-    this.test = function() { return state; };
-    if(typeof(strExp) === "string")
-        parseNumber(strExp, state);
+    
+    this.getState = function() {
+        var copyObj = {};
+        for (var i in state)
+	        copyObj[i] = state[i];
+        return copyObj;
+    };
+    
+    this.isZero = function() {
+        if(state._isNaN) return false;
+        if(!state._isFinite) return false;
+        if(state.circulating) return false;
+        if(state.integer !== '0') return false;
+        return true;
+    }
+    
+    this.add = function(other) {
+        if(!other) return this;
+        if(!(other instanceof BigDecimal))
+            return this.add(BigDecimal(other));
+        if(other.isZero()) return this;
+        var otherState = other.getState();
+        if(otherState._isNaN) return other;
+        // NaN과의 계산결과는 NaN
+        
+    };
+    
+    this.sub = function(other) {
+        
+    };
+    
+    this.multi = function(other) {
+        
+    };
+    
+    this.div = function(other) {
+        
+    };
+    
+    
+    if(typeof(strExp) === 'string' || typeof(strExp) === 'number') {
+        parseNumber(''+strExp, state);
+    }
     else if(strExp instanceof BigDecimal) {
-        // 객체의 깊은 복사 진행
+        return strExp; // BigDecimal(BigDecimal(1).add(2)) 등
     }
     else {
         throw new Error("Unexpected Object...");
@@ -62,29 +103,13 @@ function parseNumber(strExpNumber, prop) {
     if(container.length == 2) {
         prop.numerator = trimNumber_r(container[1]);
         if(prop.numerator.length === 1 && prop.numerator[0] === '0') {
-            prop.denominator = '1';
+            prop.denominator = 1;
         } else {
-            prop.denominator = '1'+'0'.repeat(prop.numerator.length);
+            prop.denominator = prop.numerator.length + 1;
             prop.numerator = trimNumber_l(prop.numerator);
         }
     }
 }
-
-BigDecimal.prototype = {
-    toString: function() {
-      return "[BigDecimal Object]";
-    }
-};
-
-BigDecimal.extend = function(props) {
-	for (var prop in props) this[prop] = props[prop];
-};
-
-//Object 의 extend를 넣어 객체의 깊은 복사가 이루어지게 하면 될듯?
-
-BigDecimal.extend({
-    PI: 3.14
-});
 
 window.BigDecimal = BigDecimal;
 }();
